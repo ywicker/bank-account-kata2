9112fa243caee4d67282de207a8f7dd342c18782
ywick <yoann.wicker@lacombedulionvert.fr>
2022-01-13 11:49:10 +0100

    US1 - Implementation des cas nominaux

--- src/main/java/com/lacombe/kata/AccountService.java
+++ src/main/java/com/lacombe/kata/AccountService.java
@@ -1,8 +1,39 @@
 package com.lacombe.kata;
 
+import java.util.HashMap;
+
 public class AccountService {
 
+	private HashMap<Integer, Integer> accounts;
+
+	public AccountService() {
+		this.accounts = new HashMap<Integer, Integer>();
+	}
+
+	/**
+	 * Permet de recuperer le solde d'un compte
+	 * Retour un solde a 0 si le compte n'exite pas
+	 * 
+	 * @param idAccount
+	 * @return le solde du compte
+	 */
+	private int getBalance(final int idAccount) {
+		if(accounts.containsKey(idAccount)) {
+			return accounts.get(idAccount);
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Deposer de l'argent sur le compte
+	 * 
+	 * @param idAccount
+	 * @param amount
+	 * @return
+	 */
 	public int deposit(final int idAccount, final int amount) {
-		return 0;
+		accounts.put(idAccount, amount + getBalance(idAccount));
+		return getBalance(idAccount);
 	}
 }
